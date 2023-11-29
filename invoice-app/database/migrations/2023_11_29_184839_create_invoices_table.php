6b46a1997a8f106f7c06c231ca010e03f098c2b8
BuiDuyHung <hungb.z98@gmail.com>
2023-11-30 02:12:18 +0700

    init database

---
+++ invoice-app/database/migrations/2023_11_29_184839_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Customer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('number')->unique();
+            $table->integer('customer_id')->unsigned();
+            $table->date('date');
+            $table->date('due_date');
+            $table->string('reference')->nullable();
+            $table->text('term_and_condition');
+            $table->double('sub_total');
+            $table->double('discount')->default(0);
+            $table->double('total');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
